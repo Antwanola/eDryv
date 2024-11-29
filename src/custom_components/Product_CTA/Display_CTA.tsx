da0baccd08b2122f8e6e15a114d207a2298ac351
Roy <95645767+RoyNkem@users.noreply.github.com>
2024-11-29 22:02:50 +0100

    Update images in Display_CTA.tsx
    Signed-off-by: Roy <95645767+RoyNkem@users.noreply.github.com>

--- src/custom_components/Product_CTA/Display_CTA.tsx
+++ src/custom_components/Product_CTA/Display_CTA.tsx
@@ -1,5 +1,8 @@
 import Product_Cta from "./Product_Cta";
-import phone from "../../assets/phone2.avif";
+import services from "../../assets/Folti_Mockups/all_services.png";
+import billings from "../../assets/Folti_Mockups/billings.png";
+import vehicle from "../../assets/Folti_Mockups/vehicle.png";
+import trip_Monitoring from "../../assets/Folti_Mockups/trip_Monitoring.png";
 
 interface ProductPropFace {
   title: string;
@@ -13,24 +16,24 @@ export default function Display_CTA() {
     {
       title: "All-in-One Services for Every Need",
       text: "Choose from a variety of services. From quick rides to vehicle rentals, reservations, and even proxy services—we’ve got it all covered.",
-      image: phone,
+      image: services,
       colour: "rgb(251, 248, 233)",
     },
     { title: "Rewards, Referrals, and More.",
       text: "Enjoy a fully customized experience. Track your Green Coin rewards, refer friends to earn more, and manage your payments and profile effortlessly.", 
-      image: phone, 
+      image: billings, 
       colour: "rgb(86, 240, 159)" 
     },
     {
       title: "Free Wi-Fi, Always Clean Rides",
       text: "Stay connected on the go with complimentary in-vehicle Wi-Fi. Plus, enjoy spotless rides every single time.",
-      image: phone,
+      image: vehicle,
       colour: "rgb(212, 255, 232)",
     },
     {
       title: "Why Ride With Us?",
       text: "With over a decade of combined experience in the industry, we’ve crafted every aspect of your journey to prioritize comfort, safety, and security. From meticulously vetted drivers to real-time trip monitoring, we’re committed to delivering the best experience, every time.",
-      image: phone,
+      image: trip_Monitoring,
       colour: "rgb(234, 255, 241)",
     }
   ];
