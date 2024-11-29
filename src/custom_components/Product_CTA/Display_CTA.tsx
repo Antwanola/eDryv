754b9ccebee863298b87008cbcf40b95c6c63185
Roy <95645767+RoyNkem@users.noreply.github.com>
2024-11-29 21:58:46 +0100

    Update Display_CTA.tsx
    Signed-off-by: Roy <95645767+RoyNkem@users.noreply.github.com>

--- src/custom_components/Product_CTA/Display_CTA.tsx
+++ src/custom_components/Product_CTA/Display_CTA.tsx
@@ -11,18 +11,28 @@ interface ProductPropFace {
 export default function Display_CTA() {
   const product_Props: Array<ProductPropFace> = [
     {
-      title: "Manage All Your Credit Cards in One Place.",
-      text: "Keep track of multiple credit cards effortlessly within the GreenBank app. Monitor balances, track transactions, and stay on top of payments without switching between different accounts",
+      title: "All-in-One Services for Every Need",
+      text: "Choose from a variety of services. From quick rides to vehicle rentals, reservations, and even proxy services—we’ve got it all covered.",
       image: phone,
       colour: "rgb(251, 248, 233)",
     },
-    { title: "Goal-Oriented Financial Planning.", text: "Set and track your financial goals with GreenBank's tailored planning tools. Whether it's saving for a vacation, a new home, or retirement, we'll help you stay on target and achieve your dreams.", image: phone, colour: "rgb(86, 240, 159)" },
+    { title: "Rewards, Referrals, and More.",
+      text: "Enjoy a fully customized experience. Track your Green Coin rewards, refer friends to earn more, and manage your payments and profile effortlessly.", 
+      image: phone, 
+      colour: "rgb(86, 240, 159)" 
+    },
     {
-      title: "Smart Spending Insights.",
-      text: "Gain valuable insights into your spending habits with GreenBank's intelligent analysis tools. Understand where your money is going, receive personalized recommendations for saving, and make informed financial decisions to achieve your goals faster.",
+      title: "Free Wi-Fi, Always Clean Rides",
+      text: "Stay connected on the go with complimentary in-vehicle Wi-Fi. Plus, enjoy spotless rides every single time.",
       image: phone,
       colour: "rgb(212, 255, 232)",
     },
+    {
+      title: "Why Ride With Us?",
+      text: "With over a decade of combined experience in the industry, we’ve crafted every aspect of your journey to prioritize comfort, safety, and security. From meticulously vetted drivers to real-time trip monitoring, we’re committed to delivering the best experience, every time.",
+      image: phone,
+      colour: "rgb(234, 255, 241)",
+    }
   ];
   return (
     <>
